f7e83cb6773c17f13e2a5ecebad12819f6b5b2c9
Jason A. Charles <93563310+jasonxcharles@users.noreply.github.com>
2025-05-18 08:49:25 +0000

    Build interactive canvas scene with images and sounds

---
+++ js/script.js
@@ -0,0 +1,59 @@
+const canvas = document.getElementById("sceneCanvas");
+const ctx = canvas.getContext("2d");
+const slider = document.getElementById("slider");
+let bgChoice = "bg1";
+
+const images = {
+  bg1: new Image(),
+  bg2: new Image(),
+  bg3: new Image(),
+  character: new Image(),
+  tree: new Image(),
+  ball: new Image(),
+  sun: new Image()
+};
+
+images.bg1.src = "images/beach.jpg";
+images.bg2.src = "images/forest.jpg";
+images.bg3.src = "images/city.jpg";
+images.character.src = "images/character.png";
+images.tree.src = "images/tree.png";
+images.ball.src = "images/ball.png";
+images.sun.src = "images/sun.png";
+
+function drawScene() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.drawImage(images[bgChoice], 0, 0, canvas.width, canvas.height);
+  ctx.drawImage(images.character, parseInt(slider.value), 250, 80, 100);
+
+  if (document.getElementById("item1").checked) {
+    ctx.drawImage(images.tree, 50, 200, 60, 80);
+  }
+  if (document.getElementById("item2").checked) {
+    ctx.drawImage(images.ball, 300, 320, 40, 40);
+  }
+  if (document.getElementById("item3").checked) {
+    ctx.drawImage(images.sun, 450, 30, 60, 60);
+  }
+}
+
+function playSound(id) {
+  const audio = document.getElementById(id);
+  audio.currentTime = 0;
+  audio.play();
+}
+
+slider.addEventListener("input", drawScene);
+document.querySelectorAll("input[name='bg']").forEach(radio => {
+  radio.addEventListener("change", (e) => {
+    bgChoice = e.target.value;
+    drawScene();
+  });
+});
+document.querySelectorAll("input[type='checkbox']").forEach(box => {
+  box.addEventListener("change", drawScene);
+});
+
+window.onload = () => {
+  drawScene();
+};
